25ed90b5fcc339f7116c3e487a1838c8e7e759d4
Brennon Fultz <brennonfultz@gmail.com>
2025-01-21 15:21:38 -0500

    Module 3 optional changes

--- app_server/controllers/meals.js
+++ app_server/controllers/meals.js
@@ -1,6 +1,9 @@
+var fs = require('fs');
+var food = JSON.parse(fs.readFileSync('./data/food.json', 'utf8'));
+
 /* GET meals view */
 const meals = (req, res) => {
-    res.render('meals', { title: 'Travlr Getaways meals'});
+    res.render('meals', { title: 'Travlr Getaways meals', food});
 };
 
 module.exports = {
--- app_server/controllers/news.js
+++ app_server/controllers/news.js
@@ -1,6 +1,10 @@
+var fs = require('fs');
+var articles = JSON.parse(fs.readFileSync('./data/articles.json', 'utf8'));
+var tips = JSON.parse(fs.readFileSync('./data/tips.json', 'utf8'));
+
 /* GET news view */
 const news = (req, res) => {
-    res.render('news', { title: 'Travlr Getaways news'});
+    res.render('news', { title: 'Travlr Getaways news', articles, tips});
 };
 
 module.exports = {
